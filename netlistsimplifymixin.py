6888808053543c784c195389cb533a2c5b1f23e5
hspfyavuz <134815486+hspfyavuz@users.noreply.github.com>
2023-06-02 01:29:44 +0200

    Update netlistsimplifymixin.py

--- netlistsimplifymixin.py
+++ netlistsimplifymixin.py
@@ -172,7 +172,7 @@ def mainprogram():
             net.draw(style='european',
                         draw_nodes=False,label_nodes=False,cpt_size=1,node_spacing=3.5)
             
-        print('_______________')
+        print('______________________________________________step'+i+''______________________________________________)
          
 
 def colored_net(net,components):
