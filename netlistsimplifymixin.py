8822e610ab55dbb010ba40a7c053985caf5add51
hspfyavuz <134815486+hspfyavuz@users.noreply.github.com>
2023-07-02 16:04:49 +0200

    Update netlistsimplifymixin.py

--- netlistsimplifymixin.py
+++ netlistsimplifymixin.py
@@ -408,13 +408,13 @@ class NetlistSimplifyMixin:
 
         keep_nodes = [str(node) for node in keep_nodes]
 
-        skip = OrderedSet()
+        skip = set()
 
         if select is not None:
-            skip = OrderedSet(self._elements) - OrderedSet(select)
+            skip = set(self._elements) - set(select)
 
         if ignore is not None:
-            skip = skip.union(OrderedSet(ignore))
+            skip = skip.union(set(ignore))
 
         # Perhaps use num cpts?
         if passes == 0:
