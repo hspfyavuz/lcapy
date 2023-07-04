0269945559b6ee9a4f5d1f081cd6382596920010
hspfyavuz <134815486+hspfyavuz@users.noreply.github.com>
2023-07-05 00:39:04 +0200

    Update minimizecalc.py

--- minimizecalc.py
+++ minimizecalc.py
@@ -140,6 +140,7 @@ def check_key_press():
     def on_button_clicked(b):
       # Display the message within the output widget.
       with output:
+         print('okay)
     
     button.on_click(on_button_clicked)
     display(button, output)
