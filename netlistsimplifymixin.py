a38f6a69425b77ae8d8805f5b9fb6ce4b69583ef
hspfyavuz <134815486+hspfyavuz@users.noreply.github.com>
2023-07-17 23:13:41 +0200

    Update netlistsimplifymixin.py

--- netlistsimplifymixin.py
+++ netlistsimplifymixin.py
@@ -31,7 +31,7 @@ class NetlistSimplifyMixin:
             
             if (subset_list[0])[0] == 'R' or (subset_list[0])[0] == 'L':
                 total += expr(self.elements[name].cpt.args[0])
-                explain_dc_series_print(self,subset_list,
+                mincalc.explain_dc_series_print(self,subset_list,
                         total,(save_new_component(subset_list[0:2],(give_net_length()+1))))
             
             if (subset_list[0])[0] == 'C':
@@ -39,29 +39,39 @@ class NetlistSimplifyMixin:
                 total = ( (expr(self.elements[subset_list[0]].cpt.args[0]) * expr(self.elements[subset_list[1]].cpt.args[0])) 
                             / (expr(self.elements[subset_list[0]].cpt.args[0]) + expr(self.elements[subset_list[1]].cpt.args[0])))
                     
-                explain_dc_parallel_print(self,subset_list,
+                mincalc.explain_dc_parallel_print(self,subset_list,
                             total,(save_new_component(subset_list[0:2],(give_net_length()+1))))
             
             if (subset_list[0])[0] == 'Z':
                 total += expr(self.elements[name].cpt.args[0])
-                explain_ac_series_print(self,subset_list,
+                mincalc.explain_ac_series_print(self,subset_list,
                         total,(save_new_component(subset_list[0:2],(give_net_length()+1))))
                     
         else:
-            if (subset_list[0])[0] == 'R' or (subset_list[0])[0] == 'C' or (subset_list[0])[0] == 'L':
+            if (subset_list[0])[0] == 'R'or (subset_list[0])[0] == 'L':
                 total = expr(0)
                 total = ( (expr(self.elements[subset_list[0]].cpt.args[0]) * expr(self.elements[subset_list[1]].cpt.args[0])) 
                             / (expr(self.elements[subset_list[0]].cpt.args[0]) + expr(self.elements[subset_list[1]].cpt.args[0])))
                     
                 mincalc.explain_dc_parallel_print(self,subset_list,
-                            total,(mincalc.save_new_component(subset_list[0:2],(mincalc.give_net_length()+1))))
+                            total,(save_new_component(subset_list[0:2],(give_net_length()+1))))
+                
+            if (subset_list[0])[0] == 'C':
+                total = expr(0)
+                total += expr(self.elements[name].cpt.args[0])
+                  
+                mincalc.explain_dc_series_print(self,subset_list,
+                            total,(save_new_component(subset_list[0:2],(give_net_length()+1))))    
+            
             if (subset_list[0])[0]=='Z':
                 total = expr(0)
                 total = ( (expr(self.elements[subset_list[0]].cpt.args[0]) * expr(self.elements[subset_list[1]].cpt.args[0])) 
                             / (expr(self.elements[subset_list[0]].cpt.args[0]) + expr(self.elements[subset_list[1]].cpt.args[0])))
+                #total = 1 / ( (1 / (expr(self.elements[subset_list[0]].cpt.args[0]))
+                #               + (1 / expr(self.elements[subset_list[1]].cpt.args[0]))))
                     
                 mincalc.explain_ac_parallel_print(self,subset_list,
-                            total,(mincalc.save_new_component(subset_list[0:2],(mincalc.give_net_length()+1))))
+                            total,(save_new_component(subset_list[0:2],(give_net_length()+1))))
                 
                 
                 
