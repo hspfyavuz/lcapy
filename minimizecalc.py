7d258d3f9f8e95ae56e3d939c510ff872e021ee5
hspfyavuz <134815486+hspfyavuz@users.noreply.github.com>
2023-07-18 01:24:53 +0200

    Update minimizecalc.py

--- minimizecalc.py
+++ minimizecalc.py
@@ -273,8 +273,15 @@ def resub():
         #newname= oldname[1:]
         #else
         for i in range(10):
-            if strimaginärteil.find('+') or strimaginärteil.find('-') >=0:
-                ergfind=(strimaginärteil.find('+') or strimaginärteil.find('-'))
+            if strimaginärteil.find('+')>=0:
+                ergfind=(strimaginärteil.find('+')
+                cnt=cnt+1
+                print(strimaginärteil)
+                strimaginärteil=strimaginärteil[ergfind+1:]
+                print(strimaginärteil)
+                print(cnt)
+             if strimaginärteil.find('-')>=0:
+                ergfind=(strimaginärteil.find('-')
                 cnt=cnt+1
                 print(strimaginärteil)
                 strimaginärteil=strimaginärteil[ergfind+1:]
