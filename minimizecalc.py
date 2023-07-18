f3c9945f27bb0b762eb856155d3d57a645ef6bf2
hspfyavuz <134815486+hspfyavuz@users.noreply.github.com>
2023-07-18 16:37:24 +0200

    Update minimizecalc.py

--- minimizecalc.py
+++ minimizecalc.py
@@ -206,7 +206,7 @@ def result_to_txt_file(number):
     pdf.set_font("Arial", size = 10)          
     f = open("Output.txt", "r")          
     for x in f:
-        pdf.cell(400, 100, txt = x, ln = 1, align = 'C')
+        pdf.cell(200, 10, txt = x, ln = 1, align = 'C')
     pdf.output("restext"+str(number+1)+".pdf")
         
 
